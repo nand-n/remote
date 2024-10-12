b51b08332f4842b1f4768b146e9b6c67a0a4cc0e
Nahi002 <nahomdebele002@gmail.com>
2024-10-12 23:46:48 +0300

    making the ui pixel perfect

--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -1,174 +1,166 @@
 "use client";
-import { Layout, Menu, MenuProps, theme } from "antd";
-import { useRouter } from "next/navigation";
-import { ReactNode, useEffect, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { ReactNode, useState } from "react";
 import { CiSettings } from "react-icons/ci";
 import { useMediaQuery } from "react-responsive";
 import { Icon } from "./commons/icon";
 
-const { Content, Sider } = Layout;
-type MenuItems = Required<MenuProps>["items"][number];
-const items: MenuItems[] = [
+interface SideBarComponentProps {
+  children: ReactNode;
+}
+
+const items = [
   {
     key: "/systems",
     label: "Systems",
-    icon: <Icon src={"/icons/folder.svg"} alt="logo" width={20} height={20} />,
+    icon: (
+      <Icon src={"/icons/folder.svg"} alt="Systems" width={20} height={20} />
+    ),
     className: "font-bold",
     children: [
       {
         key: "/system-code",
         label: "System Code",
         icon: (
-          <Icon src={"/icons/submenu.svg"} alt="logo" width={20} height={20} />
+          <Icon
+            src={"/icons/submenu.svg"}
+            alt="System Code"
+            width={20}
+            height={20}
+          />
         ),
-        className: "font-bold",
       },
       {
         key: "/properties",
         label: "Properties",
         icon: (
-          <Icon src={"/icons/submenu.svg"} alt="logo" width={20} height={20} />
+          <Icon
+            src={"/icons/submenu.svg"}
+            alt="Properties"
+            width={20}
+            height={20}
+          />
         ),
-        className: "font-bold",
       },
       {
         key: "/menues",
         label: "Menus",
         icon: (
-          <Icon src={"/icons/submenu.svg"} alt="logo" width={20} height={20} />
+          <Icon src={"/icons/submenu.svg"} alt="Menus" width={20} height={20} />
         ),
-        className: "font-bold",
       },
       {
         key: "/api-lists",
         label: "API Lists",
         icon: (
-          <Icon src={"/icons/submenu.svg"} alt="logo" width={20} height={20} />
+          <Icon
+            src={"/icons/submenu.svg"}
+            alt="API Lists"
+            width={20}
+            height={20}
+          />
         ),
-        className: "font-bold",
       },
     ],
   },
-
   {
     key: "/users-and-groups",
     label: "Users & Groups",
     icon: <CiSettings />,
-    className: "font-bold",
   },
   {
     key: "/competition",
     label: "Competition",
     icon: <CiSettings />,
-    className: "font-bold",
   },
 ];
 
-interface SideBarComponentProps {
-  children: ReactNode;
-}
-
 const SideBar: React.FC<SideBarComponentProps> = ({ children }) => {
-  const { token } = theme.useToken();
   const [collapsed, setCollapsed] = useState(false);
   const [mobileCollapsed, setMobileCollapsed] = useState(true);
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
 
   const router = useRouter();
+  const pathName = usePathname();
 
   const toggleCollapsed = () => setCollapsed(!collapsed);
 
-  const handleMenuClick = (e: { key: string }) => {
-    router.push(e.key);
+  const handleMenuClick = (e: string) => {
+    router.push(e);
     if (isMobile) {
       setMobileCollapsed(true);
     }
   };
 
-  useEffect(() => {
-    if (isMobile) {
-      setMobileCollapsed(true);
-    }
-  }, [isMobile]);
-
   return (
-    <Layout>
-      <Sider
-        theme="dark"
-        width={280}
-        style={{
-          overflow: "auto",
-          height: "100vh",
-          position: "fixed",
-          left: 0,
-          top: 0,
-          bottom: 0,
-          zIndex: 1000,
-          background: "primary",
-          transform: isMobile && mobileCollapsed ? "translateX(-100%)" : "none",
-          transition: "transform 0.3s ease",
-        }}
-        trigger={null}
-        collapsible
-        collapsed={collapsed}
-        collapsedWidth={!isMobile ? 80 : 0}
+    <div className="flex h-screen">
+      <aside
+        className={`bg-blue_gray text-white h-full transition-all duration-300 z-50 rounded-r-xl ${
+          collapsed ? "w-20" : "w-80"
+        } ${isMobile && mobileCollapsed ? "hidden" : "block"}`}
       >
-        <div className="flex justify-between p-4">
-          <div className="flex items-center gap-2">
-            {!collapsed && <Icon src={"/icons/logo.svg"} alt="logo" />}
-          </div>
-
-          <div
-            onClick={toggleCollapsed}
-            className="text-white text-xl cursor-pointer p-4 "
-          >
+        <div className="flex justify-between items-center mt-4 p-4">
+          {!collapsed && <Icon src="/icons/logo.svg" alt="Logo" />}
+          <button onClick={toggleCollapsed} className="p-2">
             {collapsed ? (
               <Icon
-                src={"/icons/expand.svg"}
-                alt="logo"
+                src="/icons/expand.svg"
+                alt="Expand"
                 width={20}
                 height={20}
               />
             ) : (
               <Icon
-                src={"/icons/collapse.svg"}
-                alt="logo"
+                src="/icons/collapse.svg"
+                alt="Collapse"
                 width={20}
                 height={20}
-                className="text-white"
               />
             )}
-          </div>
+          </button>
         </div>
-        <div className={`mt-28 text-black m-4 rounded-xl p-4 bg-blue_gray_800`}>
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={["/systems"]}
-            items={items}
-            theme="dark"
-            defaultOpenKeys={["/systems"]}
-            style={{
-              background: "transparent",
-            }}
-            inlineCollapsed={collapsed}
-            className="my-5"
-            onClick={handleMenuClick}
-          />
-        </div>
-      </Sider>
 
-      <Layout style={{ marginLeft: collapsed ? 0 : 280 }}>
-        <Content
-          style={{
-            padding: "24px",
-            minHeight: "100vh",
-            background: token.colorBgContainer,
-          }}
-        >
-          {children}
-        </Content>
-      </Layout>
-    </Layout>
+        <nav className="mt-8 mx-4">
+          <ul>
+            {items.map((item) => (
+              <li className="" key={item.key}>
+                <div
+                  className={`flex items-center p-3 ${
+                    item.label === "Systems" &&
+                    "rounded-xl rounded-b-none bg-blue_gray_800"
+                  }`}
+                >
+                  <span>{item.icon}</span>
+                  {!collapsed && <span className="ml-4">{item.label}</span>}
+                </div>
+                {item.children && !collapsed && (
+                  <ul className="rounded-xl rounded-t-none bg-blue_gray_800  pb-3">
+                    {item.children.map((subItem) => (
+                      <li
+                        key={subItem.key}
+                        onClick={() => handleMenuClick(subItem.key)}
+                        className={`flex items-center p-2 cursor-pointer py-4  rounded-2xl ${
+                          pathName === subItem.key
+                            ? "bg-lime_green text-black font-semibold  "
+                            : "hover:bg-blue_gray_800"
+                        }`}
+                      >
+                        <span>{subItem.icon}</span>
+                        <span className="ml-4 ">{subItem.label}</span>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </aside>
+
+      <main className={`transition-all duration-300 flex-1 p-6`}>
+        {children}
+      </main>
+    </div>
   );
 };
 
