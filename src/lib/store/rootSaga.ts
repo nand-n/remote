2b0a43cb3dfc6f50f89ea563489582c5e6e2da49
Nahi002 <nahomdebele002@gmail.com>
2024-10-13 23:15:31 +0300

    root saga

---
+++ src/lib/store/rootSaga.ts
@@ -0,0 +1,13 @@
+import { all } from "redux-saga/effects";
+import { watchIncrementAsync } from "./features/counter/saga";
+import {watchFetchCategoryHierarchy, watchFetchParentsCategory} from './features/categories/saga'
+import { watchTreeActions } from "./features/treeSlice/saga";
+
+export function* rootSaga(){
+    yield all([
+        watchIncrementAsync() ,
+        watchFetchCategoryHierarchy(),
+        watchFetchParentsCategory(),
+        watchTreeActions()
+    ])
+}
