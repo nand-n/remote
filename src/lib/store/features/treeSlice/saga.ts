be391fd1a7670f7f6e56c2554d048f84907cbb7e
Nahi002 <nahomdebele002@gmail.com>
2024-10-14 23:56:48 +0300

    tree data colapse and expand ui state

--- src/lib/store/features/treeSlice/saga.ts
+++ src/lib/store/features/treeSlice/saga.ts
@@ -1,16 +1,23 @@
 // src/sagas/treeSaga.ts
-import { put, takeEvery } from "redux-saga/effects";
-import { collapseAll, expandAll } from "./slice";
+import { put, takeEvery, takeLatest } from "redux-saga/effects";
+import { 
+  collapseAll,
+   expandAll } from "./slice";
 
 function* handleExpandAll(action: any) {
-  yield put(expandAll(action.payload)); 
+  try{
+    yield put(expandAll(action.payload)); 
+
+  }catch(error) {
+    console.log(error, "error happened");
+  }
 }
 
 function* handleCollapseAll() {
-  yield put(collapseAll()); 
+  yield put(collapseAll); 
 }
 
 export function* watchTreeActions() {
-  yield takeEvery(expandAll.type, handleExpandAll);
-  yield takeEvery(collapseAll.type, handleCollapseAll);
+  yield put(handleExpandAll);
+  yield put(handleCollapseAll);
 }
