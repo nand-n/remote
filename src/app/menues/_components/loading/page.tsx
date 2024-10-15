6df5f276ef10eaf64736a0f44d9369c70165338e
Nahi002 <nahomdebele002@gmail.com>
2024-10-15 23:21:47 +0300

    tree sceleton loading

--- src/app/menues/_components/loading/page.tsx
+++ src/app/menues/_components/loading/page.tsx
@@ -1,37 +1,8 @@
-// import React from "react";
-// import { Skeleton, Tree } from "antd";
-// import { DownOutlined } from "@ant-design/icons";
-
-// const LoadingTreeSkeleton: React.FC = () => {
-//   const treeData = Array.from({ length: 5 }, (_, index) => ({
-//     title: <Skeleton active paragraph={false} title={{ width: 100 }} />,
-//     key: `parent-${index}`,
-//     children: Array.from({ length: 3 }, (_, childIndex) => ({
-//       title: <Skeleton active paragraph={false} title={{ width: 80 }} />,
-//       key: `child-${index}-${childIndex}`, // Unique key for each child node
-//       children: [], // No grandchildren for simplicity
-//     })),
-//   }));
-
-//   return (
-//     <Tree
-//       showLine
-//       blockNode
-//       defaultExpandAll
-//       switcherIcon={<DownOutlined />}
-//       treeData={treeData}
-//     />
-//   );
-// };
-
-// export default LoadingTreeSkeleton;
-
 import React from "react";
 import { Skeleton, Tree } from "antd";
 import { DownOutlined } from "@ant-design/icons";
 
 const LoadingTreeSkeleton: React.FC = () => {
-  // Generate skeleton data for the tree structure
   const treeData = Array.from({ length: 3 }, (_, index) => ({
     title: (
       <div className="flex items-center">
@@ -43,7 +14,7 @@ const LoadingTreeSkeleton: React.FC = () => {
         <Skeleton active paragraph={false} title={{ width: 100 }} />
       </div>
     ),
-    key: `parent-${index}`, // Unique key for each parent node
+    key: `parent-${index}`,
     children: Array.from({ length: 3 }, (_, childIndex) => ({
       title: (
         <div className="flex items-center">
@@ -55,8 +26,8 @@ const LoadingTreeSkeleton: React.FC = () => {
           <Skeleton active paragraph={false} title={{ width: 80 }} />
         </div>
       ),
-      key: `child-${index}-${childIndex}`, // Unique key for each child node
-      children: [], // No grandchildren for simplicity
+      key: `child-${index}-${childIndex}`,
+      children: [],
     })),
   }));
 
