f15186eeb8bbfd144ecc40a61f6e7f8ea907e142
Nahi002 <nahomdebele002@gmail.com>
2024-10-19 23:34:48 +0300

    fixing layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,14 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import AntdConfigProvider from "@/lib/providers/antdProvider";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
-import SideBar from "@/components/sideBar";
-import { StoreProvider } from "@/lib/providers/storeProvider";
+import ConditionalNav from "@/lib/providers/conditionalNav";
+import ReactQueryWrapper from "@/lib/providers/reactQueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Cloit",
-  description: "Cloit the next generation",
+  title: "Fetan Eta",
+  description: "Fetan Eta next generation lucky",
   icons: {
     icon: "/icons/logo.svg",
   },
@@ -32,13 +32,13 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>
-        <AntdRegistry>
-          <AntdConfigProvider>
-            <StoreProvider>
-              <SideBar>{children}</SideBar>
-            </StoreProvider>
-          </AntdConfigProvider>
-        </AntdRegistry>
+        <ReactQueryWrapper>
+          <AntdRegistry>
+            <AntdConfigProvider>
+              <ConditionalNav>{children}</ConditionalNav>
+            </AntdConfigProvider>
+          </AntdRegistry>
+        </ReactQueryWrapper>
       </body>
     </html>
   );
