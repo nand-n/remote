fe567a1f910867ce6c1c3822a56692252ac4b065
Nahi002 <nahomdebele002@gmail.com>
2024-10-20 01:37:13 +0300

    components

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+const Hero = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Email submitted:", email);
+    setEmail(""); // Clear the email input after submission
+  };
+
+  return (
+    <section className="overflow-hidden pb-20 pt-35 md:pt-40 xl:pb-25 xl:pt-46">
+      <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
+        <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
+          <div className="md:w-1/2">
+            <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
+              🔥 Fetan Eta - A simple digital lottery.
+            </h4>
+            <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero">
+              Easy to use{" "}
+              <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark">
+                User Friendly
+              </span>
+            </h1>
+            <p className="text-gray-700 dark:text-gray-300">
+              Integrated Telegram bot - Packed with Telegram integration. Easily
+              manage lottery announcements and tickets. Insightful dashboard,
+              and fully functional gamified experience.
+            </p>
+
+            <div className="mt-10">
+              <form onSubmit={handleSubmit}>
+                <div className="flex flex-wrap gap-5">
+                  <input
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email" // Changed to "email" for better validation
+                    placeholder="Enter your email address"
+                    required // Make the input required
+                    className="rounded-full border border-stroke px-6 py-2.5 shadow-solid-2 focus:border-primary focus:outline-none dark:border-strokedark dark:bg-black dark:shadow-none dark:focus:border-primary"
+                  />
+                  <button
+                    aria-label="Get started button"
+                    type="submit" // Ensure it's a submit button
+                    className="flex rounded-full bg-black px-7.5 py-2.5 text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark dark:hover:bg-blackho"
+                  >
+                    Get Started
+                  </button>
+                </div>
+              </form>
+
+              <p className="mt-5 text-black dark:text-white">
+                Try for free, no credit card required.
+              </p>
+            </div>
+          </div>
+
+          <div className="animate_right hidden md:w-1/2 lg:block">
+            <div className="relative 2xl:-mr-7.5">
+              <Image
+                src="/images/shape/shape-01.png"
+                alt="Decorative shape"
+                width={46}
+                height={246}
+                className="absolute -left-11.5 top-0"
+              />
+              <Image
+                src="/images/shape/shape-02.svg"
+                alt="Decorative shape"
+                width={36.9}
+                height={36.7}
+                className="absolute bottom-0 right-0 z-10"
+              />
+              <Image
+                src="/images/shape/shape-03.svg"
+                alt="Decorative shape"
+                width={21.64}
+                height={21.66}
+                className="absolute -right-6.5 bottom-0 z-1"
+              />
+              <div className="relative aspect-[700/444] w-full">
+                <Image
+                  className="shadow-solid-l dark:hidden"
+                  src="/images/hero/hero-light.svg"
+                  alt="Hero"
+                  fill
+                />
+                <Image
+                  className="hidden shadow-solid-l dark:block"
+                  src="/images/hero/hero-dark.svg"
+                  alt="Hero"
+                  fill
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -3,83 +3,37 @@ import { usePathname, useRouter } from "next/navigation";
 import { ReactNode, useState } from "react";
 import { CiSettings } from "react-icons/ci";
 import { useMediaQuery } from "react-responsive";
-import { Icon } from "./commons/icon";
-import ToastMessage from "./commons/toastMessage";
+import { Icon } from "./common/icon";
+import ToastMessage from "./common/toastMessage";
+import useAuthStore from "@/lib/store/uistate/auth/login/useAuth";
+import {
+  AiOutlineClose,
+  AiOutlineMenu,
+  AiOutlineMenuFold,
+  AiOutlineMenuUnfold,
+  AiOutlineUser,
+} from "react-icons/ai";
+import { MdAnnouncement, MdDraw } from "react-icons/md";
+import { FaFolder, FaUserFriends } from "react-icons/fa";
+import { HiOutlineDocument } from "react-icons/hi";
+import { BsPeopleFill } from "react-icons/bs";
 
+interface MenuItem {
+  key: string;
+  label: string;
+  icon: React.ReactNode;
+  children?: MenuItem[]; // Optional array for submenu items
+}
 interface SideBarComponentProps {
   children: ReactNode;
 }
-
-const items = [
-  {
-    key: "/systems",
-    label: "Systems",
-    icon: (
-      <Icon src={"/icons/folder.svg"} alt="Systems" width={20} height={20} />
-    ),
-    className: "font-bold",
-    children: [
-      {
-        key: "/system-code",
-        label: "System Code",
-        icon: (
-          <Icon
-            src={"/icons/submenu.svg"}
-            alt="System Code"
-            width={20}
-            height={20}
-          />
-        ),
-      },
-      {
-        key: "/properties",
-        label: "Properties",
-        icon: (
-          <Icon
-            src={"/icons/submenu.svg"}
-            alt="Properties"
-            width={20}
-            height={20}
-          />
-        ),
-      },
-      {
-        key: "/menues",
-        label: "Menus",
-        icon: (
-          <Icon src={"/icons/submenu.svg"} alt="Menus" width={20} height={20} />
-        ),
-      },
-      {
-        key: "/api-lists",
-        label: "API Lists",
-        icon: (
-          <Icon
-            src={"/icons/submenu.svg"}
-            alt="API Lists"
-            width={20}
-            height={20}
-          />
-        ),
-      },
-    ],
-  },
-  {
-    key: "/users-and-groups",
-    label: "Users & Groups",
-    icon: <CiSettings />,
-  },
-  {
-    key: "/competition",
-    label: "Competition",
-    icon: <CiSettings />,
-  },
-];
 
 const SideBar: React.FC<SideBarComponentProps> = ({ children }) => {
   const [collapsed, setCollapsed] = useState(false);
   const [mobileCollapsed, setMobileCollapsed] = useState(true);
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  const { user } = useAuthStore();
+  const [isUserMenuOpen, setUserMenuOpen] = useState(false);
 
   const router = useRouter();
   const pathName = usePathname();
@@ -93,6 +47,53 @@ const SideBar: React.FC<SideBarComponentProps> = ({ children }) => {
     }
   };
 
+  const items: MenuItem[] = [
+    {
+      key: "/announcments",
+      icon: <MdAnnouncement />,
+      label: "Announcement",
+    },
+  ];
+
+  if (user?.role === "admin" || user?.role === "super-admin") {
+    items.push({
+      key: "/tickets",
+      label: "Tickets",
+      icon: <FaFolder />,
+      children: [
+        {
+          key: "/manage-announcment",
+          label: "Manage Announcements",
+          icon: <HiOutlineDocument />,
+        },
+        {
+          key: "/draw",
+          label: "Draw",
+          icon: <MdDraw />,
+        },
+      ],
+    });
+  }
+
+  if (user?.role === "super-admin") {
+    items.push({
+      key: "/users",
+      label: "Users & Groups",
+      icon: <BsPeopleFill />,
+      children: [
+        {
+          key: "/users/user-list",
+          label: "User List",
+          icon: <FaUserFriends />,
+        },
+        {
+          key: "/users/assign-admin",
+          label: "Assign Admins",
+          icon: <FaUserFriends />,
+        },
+      ],
+    });
+  }
   return (
     <div className="flex h-screen">
       <ToastMessage />
@@ -105,50 +106,57 @@ const SideBar: React.FC<SideBarComponentProps> = ({ children }) => {
           {!collapsed && <Icon src="/icons/logo.svg" alt="Logo" />}
           <button onClick={toggleCollapsed} className="p-2">
             {collapsed ? (
-              <Icon
-                src="/icons/expand.svg"
-                alt="Expand"
-                width={20}
-                height={20}
-              />
+              <AiOutlineMenuUnfold size={20} />
             ) : (
-              <Icon
-                src="/icons/collapse.svg"
-                alt="Collapse"
-                width={20}
-                height={20}
-              />
+              <AiOutlineMenuFold size={20} />
             )}
           </button>
         </div>
 
         <nav className="mt-8 mx-4">
           <ul>
-            {items.map((item) => (
-              <li className="" key={item.key}>
-                <div
-                  className={`flex items-center p-3 ${
-                    item.label === "Systems" &&
-                    "rounded-xl rounded-b-none bg-blue_gray_800"
-                  }`}
-                >
-                  <span>{item.icon}</span>
-                  {!collapsed && <span className="ml-4">{item.label}</span>}
-                </div>
-                {item.children && !collapsed && (
-                  <ul className="rounded-xl rounded-t-none bg-blue_gray_800  pb-3">
+            {items?.map((item) => (
+              <li key={item.key}>
+                {item?.children ? (
+                  <>
+                    <div className={`flex items-center p-3 `}>
+                      <span>{item.icon}</span>
+                      {!collapsed && <span className="ml-4">{item.label}</span>}
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    {" "}
+                    <div
+                      className={`flex items-center p-3 rounded-xl cursor-pointer ${
+                        pathName.startsWith(item.key)
+                          ? "bg-lime_green text-black font-semibold"
+                          : "hover:bg-blue_gray_800"
+                      }`}
+                      onClick={() => handleMenuClick(item.key)}
+                    >
+                      <span>{item.icon}</span>
+                      {!collapsed && <span className="ml-4">{item.label}</span>}
+                    </div>
+                  </>
+                )}
+
+                {item?.children && (
+                  <ul className="bg-blue_gray_800 rounded-xl pb-3">
                     {item.children.map((subItem) => (
                       <li
                         key={subItem.key}
                         onClick={() => handleMenuClick(subItem.key)}
-                        className={`flex items-center p-2 cursor-pointer py-4  rounded-2xl ${
+                        className={`flex items-center p-2 cursor-pointer py-4 rounded-2xl ${
                           pathName === subItem.key
-                            ? "bg-lime_green text-black font-semibold  "
-                            : "hover:bg-blue_gray_800"
+                            ? "bg-lime_green text-black font-semibold"
+                            : "hover:bg-blackBg"
                         }`}
                       >
                         <span>{subItem.icon}</span>
-                        <span className="ml-4 ">{subItem.label}</span>
+                        {!collapsed && (
+                          <span className="ml-4">{subItem.label}</span>
+                        )}
                       </li>
                     ))}
                   </ul>
@@ -160,8 +168,49 @@ const SideBar: React.FC<SideBarComponentProps> = ({ children }) => {
       </aside>
 
       <main
-        className={`transition-all duration-300 flex-1 p-6 h-full overflow-y-auto`}
+        className={`transition-all duration-300 flex-1 p-6 h-full overflow-y-auto ${
+          isMobile && "mt-4"
+        }`}
       >
+        {isMobile && (
+          <button
+            onClick={() => setMobileCollapsed(!mobileCollapsed)}
+            className="p-2 bg-blue-500 text-black fixed top-2 left-4 z-50"
+          >
+            {mobileCollapsed && <AiOutlineMenu size={20} />}
+            {!mobileCollapsed && collapsed && (
+              <div className="text-white">
+                <AiOutlineClose size={20} />
+              </div>
+            )}
+          </button>
+        )}
+        <div className="fixed top-2 right-4 z-50">
+          <button
+            className="p-1 bg-gray-200 rounded-full "
+            onClick={() => setUserMenuOpen(!isUserMenuOpen)}
+          >
+            <AiOutlineUser size={16} className="text-black" />
+          </button>
+          {isUserMenuOpen && (
+            <div className="absolute right-0 mt-2 w-48 bg-white border rounded-md shadow-lg">
+              <ul className="p-2">
+                <li className="py-2 px-4 hover:bg-gray-100 cursor-pointer">
+                  Profile
+                </li>
+                <li className="py-2 px-4 hover:bg-gray-100 cursor-pointer">
+                  Settings
+                </li>
+                <li className="py-2 px-4 hover:bg-gray-100 cursor-pointer">
+                  Notifications
+                </li>
+                <li className="py-2 px-4 hover:bg-gray-100 cursor-pointer">
+                  Logout
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
         {children}
       </main>
     </div>
