68950f7930e167d3f22dcb6566fd7a2e2c82d48e
Nahi002 <nahomdebele002@gmail.com>
2024-10-20 01:34:49 +0300

    annoucnemtn

---
+++ src/app/(afterLogin)/announcments/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+import AnnouncementList from './_components/announcmentList';
+
+const Announcements = () => {
+  return (
+    <div className="container mx-auto py-4  ">
+      <AnnouncementList />
+    </div>
+  );
+};
+
+export default Announcements;
